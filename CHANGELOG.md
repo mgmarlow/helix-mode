35aeadab67d73be012a3ae247af9a26ddc2cd746
Graham Marlow <graham@mgmarlow.com>
2025-03-09 13:06:57 -0700

    Package metadata and changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 0.1.0 (2025-03-09)
+
+Initial release! Only supporting a minimal subset of my most-used
+Helix keybindings. Mostly the basics.
--- helix.el
+++ helix.el
@@ -4,6 +4,9 @@
 
 ;; Author: Graham Marlow
 ;; Keywords: convenience
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "25.1"))
+;; URL: https://github.com/mgmarlow/helix-mode
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
